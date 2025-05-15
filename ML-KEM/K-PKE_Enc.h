065c3ccf3da9a799e5df4ad3901aeccdbd0a8428
samdae5back <109176541+samdae5back@users.noreply.github.com>
2025-05-15 18:32:22 +0900

    Delete ML-KEM directory

--- ML-KEM/K-PKE_Enc.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void K_PKE_Enc(unsigned char** ek_pke, unsigned char* message, unsigned char* randomness, unsigned char** ciphertext);
--- ML-KEM/K-PKE_KeyGen.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-void K_PKE_KeyGen(unsigned char* d, unsigned char** ek_pke, unsigned char** dk_pke);
