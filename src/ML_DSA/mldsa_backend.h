41a9e4995dd7f520710dfc37ff25681aacb7d311
Myungjun Kim <109176541+samdae5back@users.noreply.github.com>
2026-08-27 02:02:04 +0900

    Declare portable ML-DSA multilevel backend

---
+++ src/ML_DSA/mldsa_backend.h
@@ -0,0 +1,19 @@
+#ifndef CRYPTO_INTERNAL_MLDSA_BACKEND_H
+#define CRYPTO_INTERNAL_MLDSA_BACKEND_H
+
+#define MLD_CONFIG_PARAMETER_SET 44
+#include "mldsa_native.h"
+#undef MLD_CONFIG_PARAMETER_SET
+#undef MLD_H
+
+#define MLD_CONFIG_PARAMETER_SET 65
+#include "mldsa_native.h"
+#undef MLD_CONFIG_PARAMETER_SET
+#undef MLD_H
+
+#define MLD_CONFIG_PARAMETER_SET 87
+#include "mldsa_native.h"
+#undef MLD_CONFIG_PARAMETER_SET
+#undef MLD_H
+
+#endif
